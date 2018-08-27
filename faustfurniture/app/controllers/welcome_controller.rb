536161794901631e90c16a7b57acfba32a046ebb
gene-radcliffe <g_radcliffe@hotmail.com>
2018-08-26 23:33:37 -0400

    made minor updates in my code

--- faustfurniture/app/controllers/welcome_controller.rb
+++ faustfurniture/app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   include CategoriesHelper
   def index
-    @categories_hash={:tables => "tables",:seating => "seating",:bedroom => "bedroom", :storage=>"storage",:desks=>"desks",:miscellaneous=> "miscellaneous"}
+    
     @categories = ["tables","seating","bedroom", "storage","desks", "miscellaneous"]
   end
 end
